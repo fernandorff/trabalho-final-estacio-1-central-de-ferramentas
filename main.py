d07447e6c3a060f43d63aa2b89a0cca1bc540f0a
MariLFO <mari.fer.onorio@gmail.com>
2022-07-04 20:14:34 -0700

    Ler e gravar utilizando a classe Tecnico

--- main.py
+++ main.py
@@ -4,10 +4,20 @@ import csv
 databasePath = "database.csv"
 
 
+class Tecnico:
+    def __init__(self, id_cpf, nome, sobrenome, telefone, turno, equipe):
+        self.id_cpf = id_cpf
+        self.nome = nome
+        self.sobrenome = sobrenome
+        self.telefone = telefone
+        self.turno = turno
+        self.equipe = equipe
+
+
 def main():
     rows = [
-        {'nome': 'João', 'sobrenome': 'Da Silva', 'telefone': 123456789, 'turno': 'M', 'equipe': 'B'},
-        {'nome': 'Maria', 'sobrenome': 'Dos Santos', 'telefone': 111111111, 'turno': 'M', 'equipe': 'B'},
+        Tecnico(12345678901, "João", "Da Silva", 123456789, 'M', 'B').__dict__,
+        Tecnico(11111111111, "Maria", "Dos Santos", 111111111, 'M', 'B').__dict__
     ]
     write(rows)
     read()
